233cb80f7d5be5514f53b64492bd03fcbd178a20
Your Name <suttonk@bcglobal.net>
2021-04-13 19:13:43 -0700

    heroku changes

--- server.js
+++ server.js
@@ -31,7 +31,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb2", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb2", { useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false });
 
 app.use(
   cors({
